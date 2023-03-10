0196ebfb8b518f560dba1003507715b3ff4ad241
ViaChessLove <85558513+ViaChessLove@users.noreply.github.com>
2023-03-10 10:29:44 +0500

    почти дописал приложение + написал часть реализации в дипломе

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Dot, LoadingWrapper } from './styles';
 
+const DOTS = [
+  '0s',
+  '0.05s',
+  '0.1s',
+  '0.15s',
+  '0.2s',
+  '0.25s',
+  '0.3s',
+]
+
 const Loader = () => (
   <LoadingWrapper>
-    <Dot delay="0s" />
-    <Dot delay="0.05s" />
-    <Dot delay="0.1s" />
-    <Dot delay="0.15s" />
-    <Dot delay="0.2s" />
-    <Dot delay="0.25s" />
-    <Dot delay="0.3s" />
+    {DOTS.map((delay, delayIndex) => (
+      <Dot key={delayIndex} delay={delay} />
+    ))}
   </LoadingWrapper>
 );
 
