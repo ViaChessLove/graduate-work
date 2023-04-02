bcc0208ac61e7b073da306aee2b81e87ece96610
ViaChessLove <85558513+ViaChessLove@users.noreply.github.com>
2023-04-02 13:14:27 +0500

    1

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import App from './App';
 import { Provider } from 'react-redux';
 
 import { store } from './redux/store';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 ReactDOM
   .createRoot(document.getElementById('root') as HTMLElement)
